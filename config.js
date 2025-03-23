838ceb3add9ce5148cde3e22b99f5b9f8c8421c2
kyy•botzV3 <118798675+RizkiBotz@users.noreply.github.com>
2025-03-24 03:50:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,115 @@
+
+const chalk = require("chalk")
+const fs = require("fs")
+global.namastore = "KYYSTORE DOESN'T HAVE A SHOP"
+ global.namaowner = "ᴋʏʏ-sᴛᴏʀᴇ"
+  global.ownername = "ᴋʏʏ-sᴛᴏʀᴇ"
+global.ownerNumber = ["6288742782469@s.whatsapp.net"]
+global.owner = ['6288742782469']
+global.namaBot = " ᴋʏʏ-sᴛᴏʀᴇ"
+global.namabot = " ᴋʏʏ-sᴛᴏʀᴇ"
+global.nomerOwner = "6288742782469"
+global.nomorOwner = ['6288742782469']
+global.botnum = ['6288742782469']
+global.namaDeveloper = "IG: ikyy_joki08 " //jangn diubh bng pelisss
+global.packname = " 🅥ᴋʏʏ"
+global.namach = "🪪 ᴋʏʏ-sᴛᴏʀᴇ"
+global.descown = "By WhatsApp Bot Indonesia"
+global.author = "Sticker By:\n@ᴋʏʏ-sᴛᴏʀᴇ "
+global.thumb = fs.readFileSync("./thumb.png")
+global.tqq = fs.readFileSync("./ShoNheMedia/image/tqto.png")
+global.smple = fs.readFileSync("./ShoNheMedia/image/simple.jpg")
+global.potone = fs.readFileSync("./ShoNheMedia/image/owner.jpg")
+global.sessionName = 'session'
+global.version = "3.3.5"
+global.forpdf = fs.readFileSync("./urpdf.jpg")
+global.forpdf2 = fs.readFileSync("./urpdf2.jpg")
+global.tempatDB = 'database.json';
+global.itg = "🅥ᴋʏʏ-sᴛᴏʀᴇ"
+global.crown = "🌷 ᴋʏʏ-sᴛᴏʀᴇ"
+global.ytbb = "ᴋʏʏ-sᴛᴏʀᴇ"
+//━━━━━━━━━━━━━━━[ PAYMENT SETTING 
+global.nomordana = "6288279203341" //nomer danamu (klo gak ada kosongin cuok
+global.namadana = "Rizki Aji Setya" //nama sesuai nomer dana
+global.nomorgopay = "6288279203341" //no gopay (klo gak ada kosongin cuok
+global.namagopay = "Sesuaikan dengan Nama Gopay"
+global.nomorovo = "6288279203341" //no ovo (klo gak ada kosongin cuok
+global.namaovo = "Sesuaikan dengan Nama Ovo"
+global.nomorshopepay = "6288279203341" //no shopeepay (klo gak ada kosongin cuok
+global.namashopepay = "Lintang Pratama Aditya K"
+global.nomorseabank = "901484264240" //Klo punya akun REKENING BANK SEABANK (klo gak ada kosongin cuok
+global.namaseabank = "Lintang Pratama Aditya K"
+global.nomorbca = "1230903219" // NO REKENING BANK BCA (klo gak ada kosongin cuok
+global.namabca = "Lintang Pratama Aditya K"
+global.nomordanamon = "903684075934" //NO REKENING BANK DANAMON (klo gak ada kosongin cuok
+global.namadanamon = "Lintang Pratama Aditya K"
+global.ttk = "kyy"
+global.themeemoji = "🔥"
+global.typereply = "s4" //s1 - s5
+global.typemenu = "s1" //s1 - s12
+global.tmn = fs.readFileSync("./ShoNheMedia/image/tmenu.jpg")
+global.tekspushkon = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+global.saluran = ""
+global.idsaluran = ""
+global.gh = "https://github.com/RizkiBotz/kyy_store"
+global.wagc = 'https://chat.whatsapp.com/GQ5Gp0eSeDS6dPBYeHE6kf'
+global.wach = ""
+global.ibbKey = "e73afc3e94477852c77e41a41572fa8d"
+// Apikey
+global.APIs = {
+	alfa: 'https://api.zeeoneofc.my.id', //ganti sesukamu
+}
+global.APIKeys = {
+	'https://api.zeeoneofc.my.id': 'JKqddGlPzEcod4P', //ini juga
+}
+global.thum = fs.readFileSync("./storage/image.png") 
+global.imqris = fs.readFileSync ("./storage/qris.png")
+//
+global.mess = {
+    admins: "⛔ *[AKSES DITOLAK]* ────\n🚨 *PERINGATAN SISTEM* 🚨\n❌ *Identitas Tidak Terverifikasi!*\n\n📡 Hanya ADMIN yang memiliki akses eksekusi perintah ini.\n🔐 *Tingkat Otorisasi: RENDAH*\n\n🛑 *Akses Anda Telah Diblokir!*",
+    
+    owners: "🚫 *[AKSES ILEGAL]* ────\n⚠️ *INTRUSION DETECTED* ⚠️\n🕵️‍♂️ *Upaya eksekusi perintah oleh entitas tak dikenal!*\n\n🔑 *Hanya MEMBER PREMIUM sistem yang memiliki otoritas penuh!*\n🛡️ *Akses Ditolak Secara Permanen!*",
+    
+    dones: "✅ *[OPRASI SELESAI]* ────\n💾 *Data berhasil diproses dengan akurasi tinggi.*\n📊 *Status: 100% SUCCESS*\n\n🔄 *Mengalihkan ke misi berikutnya...*",
+    
+    abots: "🤖 *[ERROR SISTEM]* ────\n💢 *Kegagalan Otorisasi!*\n🛠️ *Bot tidak memiliki hak akses sebagai admin!*\n\n🛑 *Tingkatkan kredensial atau hubungi pemilik sistem!*",
+    
+    groups: "📂 *[OPERASI TIDAK VALID]* ────\n🚧 *Komando ini hanya diizinkan dalam jaringan GRUP.*\n\n🖥️ *Silakan gunakan di ruang komunikasi yang tepat!*",
+    
+    privates: "🔒 *[ZONA AMAN]* ────\n🕵️ *Komando ini bersifat PRIVAT dan hanya bisa digunakan dalam komunikasi langsung!*\n\n⚠️ *Hindari penyalahgunaan informasi rahasia!*",
+    
+    vips: "🛑 *[AKSES TERBATAS]* ────\n🎟️ *Mode VIP Diperlukan!*\n💎 *Hanya pengguna ELITE yang dapat menggunakan fitur ini!*\n\n📡 *Silakan tingkatkan status Anda untuk membuka akses premium.*",
+    
+    waits: "*⏳PROSES BERLANGSUNG.....*\n😇MOHON BERSABAR",
+    
+    bands: "⚠️ *[PERINGATAN SISTEM]* ────\n❌ *Entitas Terblokir!*\n🛑 *Akses ditolak oleh pemilik sistem!*\n\n🚫 *Upaya lebih lanjut akan ditandai sebagai ancaman!*",
+    
+    regis: "⚠️ *[ALARM SISTEM]* ────\n🔍 *Entitas Tak Terdaftar Terdeteksi!*\n📝 *Silakan lakukan registrasi untuk mendapatkan akses penuh.*\n\n📎 *Klik tombol di bawah untuk mendaftar!*"
+}
+global.pterodactylkey = {
+    domain: "",
+    apikey: "",
+    capikey: "",
+    eggs: "15",
+    location: "1"
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 20
+}
+global.APIs = {
+	hitori: 'https://my.hitori.pw/api',
+}
+global.APIKeys = {
+	'https://my.hitori.pw/api': 'htrkey-HysnOetF',
+}
+
+let file = require.resolve(__filename) 
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
